6ba811e3f30fe8915142268514d1736eb9c3638a
KatKmiotek <katarzyna.kmiotek@yahoo.com>
2021-09-01 08:05:29 +0100

    working with mutation

--- script.js
+++ script.js
@@ -2,22 +2,19 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 
+// export let options = {
+//   stages: [
+//     { duration: '30s', target: 20 },
+//     { duration: '1m30s', target: 10 },
+//     { duration: '20s', target: 0 },
+//   ],
+// };
 
-export let options = {
-  stages: [
-    { duration: '30s', target: 20 },
-    { duration: '1m30s', target: 10 },
-    { duration: '20s', target: 0 },
-  ],
-};
+ 
+const accessToken = __ENV.GITHUB_TOKEN;
+const issueID = "MDU6SXNzdWU5ODM1MTQ0MDg=";
 
-
-
-// let accessToken = __ENV.GITHUB_TOKEN;
-// console.log('github token is', accessToken);
-const accessToken = __ENV.GITHUB_TOKEN
-
-let query = `
+const query = `
 {
       repository(name: "perf", owner: "KatKmiotek") {
         issue(number: 1) {
@@ -32,15 +29,29 @@ let query = `
     }
   }`;
 
-let headers = {
+  const mutation = `
+  mutation MyMutation {
+    gitHub {
+      addComment(input: { subjectId: "${issueID}", body: "hello" }){
+        subject{
+          id
+        }
+      }
+    }
+  }
+  
+  
+  `;
+
+const headers = {
   Authorization: `Bearer ${accessToken}`,
   'Content-Type': 'application/json',
 };
 
 
 export default function () {
-    let res = http.post('https://api.github.com/graphql', JSON.stringify({ query: query }), { headers: headers });
-    let comments = JSON.parse(res.body).data.repository.issue.comments.edges;
+    const res = http.post('https://api.github.com/graphql', JSON.stringify({ query: query }), { headers: headers });
+    const comments = JSON.parse(res.body).data.repository.issue.comments.edges;
     check(res, 
         {
         "status is 200": () => res.status === 200,
@@ -48,4 +59,7 @@ export default function () {
         "has correct comment 2": () => comments[1].node.bodyText === "another one",
         "has correct comment 3": () => comments[2].node.bodyText === "3rd one",
     });
+
+    const resMut = http.post('https://api.github.com/graphql', JSON.stringify({ query: mutation }), { headers: headers })
+    console.log('response mutation', JSON.stringify(resMut.body));
 }
